4cd0ecd2f4fadaa772314a8788eeb8016e01c530
Davi Estrella <sttar@debian-sttar.@dev>
2023-11-07 22:28:11 -0300

    Mudei a disposição da home. Adcionei o botão de login e cadastro

---
+++ convites-digitais/src/app/pages/cadastro/cadastro.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CadastroComponent } from './cadastro.component';
+
+
+
+@NgModule({
+  declarations: [
+    CadastroComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CadastroModule { }
